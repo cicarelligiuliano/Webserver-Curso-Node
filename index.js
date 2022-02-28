e0717ff42a2a14e02bbd143417d058507be35f85
cicarelligiuliano <cicarelligiuliano@outlook.com.ar>
2022-02-27 21:41:20 -0300

    Prueba react

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ require("dotenv").config();
 
 const port = process.env.PORT;
 
-app.set("view engine", "hbs");
+app.set("view engine");
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    nombre: "Giuliano Cicarelli",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.render("home", {
+//     nombre: "Giuliano Cicarelli",
+//     titulo: "Curso de Node",
+//   });
+// });
 
-app.get("/generic", (req, res) => {
-  res.render("generic", {
-    nombre: "Giuliano Cicarelli",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/generic", (req, res) => {
+//   res.render("generic", {
+//     nombre: "Giuliano Cicarelli",
+//     titulo: "Curso de Node",
+//   });
+// });
 
-app.get("/elements", (req, res) => {
-  res.render("elements", {
-    nombre: "Giuliano Cicarelli",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/elements", (req, res) => {
+//   res.render("elements", {
+//     nombre: "Giuliano Cicarelli",
+//     titulo: "Curso de Node",
+//   });
+// });
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/404.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, console.log(`corriendo en puerto ${port}`));
